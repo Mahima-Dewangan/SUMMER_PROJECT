0260c5a2c20fe81175ee2bba90cf3ac6c1bcf0c4
Mahima-Dewangan <mahimadewangan11.com>
2025-07-10 06:24:09 -0700

    third commit

--- PARTITION_2/SQL_QUERIES/QF_SQL_Queries.sql
+++ PARTITION_2/SQL_QUERIES/QF_SQL_Queries.sql
@@ -1,22 +1,23 @@
--------Query 1   Red (P3) -------------
+-------Query 1   Red (P5) -------------
 SELECT * FROM customer
-WHERE c_acctbal BETWEEN 4000 AND 6000
-  AND c_nationkey BETWEEN 13 AND 20;
+WHERE c_acctbal BETWEEN 5000 AND 6000         
+  AND c_nationkey BETWEEN 15 AND 20
 
 
--------QUERY 2  Green (P1 , P2 , P3 ,P5)
+-------QUERY 2  Blue (P1 , P2 , P3 ,P5)   P5
 SELECT * FROM customer
 WHERE c_acctbal BETWEEN 1500 AND 6500
   AND c_nationkey BETWEEN 10 AND 20;
 
 
----------QUERY 3  Blue (ALL PARTITIONS)
+---------QUERY 3  Green (ALL PARTITIONS)   
 SELECT * FROM customer
 WHERE c_acctbal BETWEEN 500 AND 8500
   AND c_nationkey BETWEEN 2 AND 22;
 
------------QUERY 4 ORANGE (P2 , P5)
+-----------QUERY 4 Purple (P1 , P3 , P4)     
 SELECT * FROM customer
-WHERE c_acctbal BETWEEN 100 AND 6100  
+WHERE c_acctbal BETWEEN 100 AND 8100
+   AND c_nationkey BETWEEN 1 AND  11
 
 
